9ca4f46e3e08c45cc061178e6d41b9a5dfd2aaf0
Victor Lopez (UST <U34006@ust-global.com>
2018-03-07 11:56:57 -0600

    Angular Material modules

--- sobre-front/src/app/app.module.ts
+++ sobre-front/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
+    BodyComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTableModule
   ],
   schemas:[ NO_ERRORS_SCHEMA ],
   providers: [],
