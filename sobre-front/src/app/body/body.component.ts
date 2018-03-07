af78ede0feefa944ef7862ac75f8a4daab211d1f
Victor Lopez (UST <U34006@ust-global.com>
2018-03-07 14:39:37 -0600

    - rowHandler to rowUIHandler - editRowBudget() to editBudget() - interfaces updated adding/removing checkedBox/check

--- sobre-front/src/app/body/body.component.ts
+++ sobre-front/src/app/body/body.component.ts
@@ -8,62 +8,59 @@ export class BodyComponent {
   private title = 'El Sobre';
 
   private dataSource = SOBRE_DATA;
-  private rowHandler = SOBRE_ROW_DATA;
+  private rowUIHandler = SOBRE_ROW_UI;
   private columnsToDisplay = ['checkBox', 'name', 'budget', 'adjust'];
 
   private nuBudget = 100 ;
 
-  editRowBudget(id): void {
+  editBudget(id): void {
     let name: string = "Edit";
     let color: string = "primary";
 
-    if (!this.rowHandler[id].displayInput) {
+    if (!this.rowUIHandler[id].displayInput) {
       name  = "Done";
       color = "warn";
     }
 
-    this.rowHandler[id].displayInput = !this.rowHandler[id].displayInput;
-    this.rowHandler[id].buttonName   = name;
-    this.rowHandler[id].buttonColor  = color;
+    this.rowUIHandler[id].displayInput = !this.rowUIHandler[id].displayInput;
+    this.rowUIHandler[id].buttonName   = name;
+    this.rowUIHandler[id].buttonColor  = color;
   }
 
-  checkedRow(id): void {
-    this.dataSource[id].check = !this.dataSource[id].check;
+  checkRow(id): void {
+    this.rowUIHandler[id].checkedBox = !this.rowUIHandler[id].checkedBox;
   }
 
   setToZero(): void {
-    this.dataSource.forEach((data) => {
-      if (data.check) {
-        data.budget = 0;
-        // data.check = false; TODO: Uncheck the box automatically
+    this.rowUIHandler.forEach((row, index) => {
+      if (row.checkedBox) {
+        this.dataSource[index].budget = 0;
       }
     });
-
-    console.info(this.dataSource);
   }
 };
 
 export interface Sobre {
-  check: boolean;
   id: number;
   name: string;
   budget: number;
 };
 
-export interface SobreRow {
+export interface SobreRowUI {
+  checkedBox: boolean;
   displayInput: boolean;
   buttonName: string;
   buttonColor: string;
 };
 
 const SOBRE_DATA: Sobre[] = [
-  {check: false, id: 0, name: 'Rent', budget: 1200},
-  {check: false, id: 1, name: 'Car', budget: 1500},
-  {check: false, id: 2, name: 'Cats', budget: 200}
+  {id: 0, name: 'Rent', budget: 1200},
+  {id: 1, name: 'Car', budget: 1500},
+  {id: 2, name: 'Cats', budget: 200}
 ];
 
-const SOBRE_ROW_DATA: SobreRow[] = [
-  {displayInput: false, buttonName: "Edit", buttonColor: "primary"},
-  {displayInput: false, buttonName: "Edit", buttonColor: "primary"},
-  {displayInput: false, buttonName: "Edit", buttonColor: "primary"}
+const SOBRE_ROW_UI: SobreRowUI[] = [
+  {checkedBox: false, displayInput: false, buttonName: "Edit", buttonColor: "primary"},
+  {checkedBox: false, displayInput: false, buttonName: "Edit", buttonColor: "primary"},
+  {checkedBox: false, displayInput: false, buttonName: "Edit", buttonColor: "primary"}
 ];
