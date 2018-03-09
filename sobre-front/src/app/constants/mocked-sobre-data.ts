4ee816adc3f705c475a23aa09e0eb7efe410f09b
MijaelWatts <elyon.systems.mijael@gmail.com>
2018-03-08 22:47:58 -0600

    Mocked data set to budget zero.

--- sobre-front/src/app/constants/mocked-sobre-data.ts
+++ sobre-front/src/app/constants/mocked-sobre-data.ts
@@ -4,7 +4,7 @@ import { Sobre } from '../interfaces/sobre';
  * Mocked data to display in the table.
  */
 export const MOCKED_SOBRE_DATA: Sobre[] = [
-  {id: 0, name: 'Xpense01', budget: 10},
-  {id: 1, name: 'Xpense02', budget: 20},
-  {id: 2, name: 'Xpense03', budget: 30}
+  {id: 0, name: 'Xpense01', budget: 0},
+  {id: 1, name: 'Xpense02', budget: 0},
+  {id: 2, name: 'Xpense03', budget: 0}
 ]
